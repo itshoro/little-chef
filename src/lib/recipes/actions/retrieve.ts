566fc401b0b21193597fde47520979afc7be23ac
Tim Nelke <tim.nelke@outlook.com>
2023-11-23 21:43:15 +0100

    add get single recipe action

--- src/lib/recipes/actions/retrieve.ts
+++ src/lib/recipes/actions/retrieve.ts
@@ -1,17 +1,53 @@
+"use server";
+
 import { getConnection } from "@/lib/db";
 import { ExecutedQuery } from "@planetscale/database";
+import { Recipe } from "../validators";
 
 type RecipeOverview = { name: string; id: string };
 
-function toRecipe<Row = ExecutedQuery["rows"][number]>(row: Row) {
+function toRecipeOverview<Row = ExecutedQuery["rows"][number]>(row: Row) {
   return { id: row["id"], name: row["name"] } satisfies RecipeOverview;
+}
+
+function toRecipe<Row = ExecutedQuery["rows"][number]>(
+  recipeRow: Row,
+  stepRows: Row[]
+) {
+  return {
+    id: recipeRow["id"],
+    name: recipeRow["name"],
+    servings: recipeRow["servings"],
+    totalDuration: "",
+    steps: stepRows.map((row) => row["description"]),
+  } satisfies Recipe;
 }
 
 async function getOverview() {
   const connection = getConnection();
   const result = await connection.execute("SELECT `id`, `name` FROM recipes");
 
-  return result.rows.map((row) => toRecipe(row));
+  return result.rows.map((row) => toRecipeOverview(row));
 }
 
-export { getOverview };
+async function get(id: string) {
+  const connection = getConnection();
+
+  const [recipeResult, stepResults] = await Promise.all([
+    connection.execute(
+      "SELECT `id`, `name`, `servings` FROM recipes WHERE id = :id",
+      { id }
+    ),
+    connection.execute("SELECT * FROM recipe_steps WHERE parent_id = :id", {
+      id,
+    }),
+  ]);
+
+  if (recipeResult.size === 0 || stepResults.size === 0) {
+    throw new Error("Recipe not found.");
+  }
+
+  return toRecipe(recipeResult.rows[0], stepResults.rows);
+}
+
+export { get, getOverview };
