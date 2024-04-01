6dff385978cd2bff905a87805b6ade18459a5c78
Tim Nelke <tim.nelke@outlook.com>
2024-04-01 22:06:54 +0200

    start implementing new design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,38 @@ import { CTALink } from "./components/CallToAction/Link";
 import { SearchInput } from "./components/Search/Input";
 import { RecipeOverview } from "./components/Recipe/Overview";
 import Link from "next/link";
+import { validateRequest } from "@/lib/auth/lucia";
+import { Collection, User } from "@prisma/client";
+import { getPrismaClient } from "@/lib/prisma";
+import { Header } from "./components/header/header";
+import * as Input from "./components/input";
 
 const Page = async () => {
+  const { user } = await validateRequest();
+
   return (
-    <div
-      className="magic-grid h-[100svh]"
-      style={{
-        gridTemplateRows:
-          "[header-start] min-content [header-end content-start] 1fr [content-end action-start] min-content [action-end]",
-      }}
-    >
-      <header
-        className="border-b"
-        style={{ gridArea: "header", gridColumn: 1 }}
-      >
-        <div className="p-4 pb-2">
-          <div className="flex items-center justify-between">
-            <SearchInput />
-            <img
-              src="/timnelke.jpg"
-              className="rounded-full aspect-square w-10 border-2 border-white"
-              alt=""
-            />
-          </div>
-        </div>
-      </header>
-      <div className="p-2 overflow-y-auto h-full">
-        <RecipeList />
-      </div>
-      <footer
+    <>
+      <Header />
+      <section className="p-4">
+        <SearchInput />
+      </section>
+      <section className="px-4 py-3 border-y border-stone-200">
+        <ContentFilter />
+      </section>
+      <main>
+        <header className="px-4 py-5">
+          <h1 className="flex items-center">
+            <span className="inline-block size-2 mr-2.5 bg-lime-500 rounded-full" />{" "}
+            <span className="text-sm font-medium">Your Saved Collections</span>
+          </h1>
+        </header>
+      </main>
+      <CollectionList user={user} />
+      {/* <div className="p-2 overflow-y-auto h-full"> */}
+
+      {/* <RecipeList /> */}
+      {/* </div> */}
+      {/* <footer
         className="border-t w-full flex p-4"
         style={{ gridArea: "action", gridColumn: 1 }}
       >
@@ -48,40 +51,90 @@ const Page = async () => {
             Add
           </CTALink>
         </div>
-      </footer>
-    </div>
+      </footer> */}
+    </>
   );
 };
 
-const NoRecipes = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-full font-medium text-center gap-4 select-none">
-      <div className="text-6xl">🍽️</div>
-      <div className="text-lg">You haven't stored a recipe yet.</div>
-      <hr className="bg-stone-200 w-24" />
-
-      <Link className="underline text-green-600" href="/recipes/add">
-        How about adding a new one?
-      </Link>
-    </div>
-  );
-};
-
-const RecipeList = async () => {
-  let recipeList = await getOverview();
-  if (recipeList.length === 0) return <NoRecipes />;
+const ContentFilter = () => {
+  const route = "/collections";
 
   return (
-    <ul>
-      {recipeList.map((recipe) => (
-        <li
-          key={recipe.publicId}
-          className="p-4 shadow-sm bg-white rounded-lg mb-2"
+    <ul className="flex">
+      <li className="flex-1 flex items-center">
+        <Link
+          data-active={route === "/collections"}
+          className="flex-1 font-medium p-3 rounded-full data-[active=true]:bg-stone-100 text-center"
+          href="/collections"
         >
-          <RecipeOverview recipe={recipe} />
-        </li>
-      ))}
+          Collections{" "}
+          <span className="font-normal text-stone-400">&middot; 2</span>
+        </Link>
+      </li>
+      <li className="flex-1 flex items-center">
+        <Link
+          data-active={route === "/recipes"}
+          className="flex-1 font-medium p-3 rounded-full data-[active=true]:bg-stone-100 text-center"
+          href="/recipes"
+        >
+          Recipes{" "}
+          <span className="font-normal text-stone-400">&middot; 249</span>
+        </Link>
+      </li>
     </ul>
+  );
+};
+
+const CollectionList = async ({ user }: { user: User | null }) => {
+  if (!user) return null;
+  const client = getPrismaClient();
+
+  const collections = await client.collection.findMany({
+    where: { userId: user.id },
+  });
+
+  console.log(collections, user.id);
+
+  return (
+    <ul className="grid gap-3 px-4">
+      {collections.map((collection) => {
+        return (
+          <li key={collection.id} className="">
+            <a href={`/collections/${collection.id}`}>
+              <CollectionCard collection={collection} />
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const CollectionCard = ({ collection }: { collection: Collection }) => {
+  return (
+    <article className="rounded-xl bg-stone-100 shadow-sm">
+      <div className="px-4 py-5">
+        <div className="flex justify-between items-center">
+          <div className="font-medium">
+            {collection.name}{" "}
+            <div className="inline-block font-normal text-sm">
+              <span className="text-stone-400">&middot;</span>{" "}
+              <span className="text-lime-500">15 Recipes</span>
+            </div>
+          </div>
+          <div>
+            <img
+              className="block size-10 rounded-full border-2 border-white"
+              src="/timnelke.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <p className="text-stone-600 pt-3 ">
+          Some description blalalala ipsumipsumipsumipsumipsum ipsum
+        </p>
+      </div>
+    </article>
   );
 };
 
