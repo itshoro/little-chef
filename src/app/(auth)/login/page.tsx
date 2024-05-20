5eb047a29b7b72ebdb16b80d2a5da845ce67ca91
Tim Nelke <tim.nelke@outlook.com>
2024-05-20 17:45:19 +0200

    login page now properly utilizes dal

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,34 @@
-const Page = () => {
-  return <></>;
+import NextLink from "next/link";
+import { login } from "./action";
+import * as Input from "@/app/components/input";
+import { Submit } from "@/app/(search)/recipes/components/RecipeForm/Form";
+
+const LoginPage = async () => {
+  return (
+    <>
+      <h1 className="font-medium">Login</h1>
+      <form className="py-4" action={login}>
+        <div className="pb-2">
+          <Input.Root name="username">
+            <Input.Label>Username</Input.Label>
+            <Input.Group>
+              <Input.Element type="text" />
+            </Input.Group>
+          </Input.Root>
+        </div>
+        <div className="pb-6">
+          <Input.Root name="password">
+            <Input.Label>Password</Input.Label>
+            <Input.Group>
+              <Input.Element type="password" />
+            </Input.Group>
+          </Input.Root>
+        </div>
+        <Submit>Continue</Submit>
+      </form>
+      <NextLink href="/sign-up">Create an account</NextLink>
+    </>
+  );
 };
 
-export default Page;
+export default LoginPage;
