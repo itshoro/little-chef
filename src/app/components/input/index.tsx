4c23a9cce96914fbe927d769bfa01cfeb72dbab4
Tim Nelke <tim.nelke@outlook.com>
2024-07-26 23:59:49 +0200

    remove select components

--- src/app/components/input/index.tsx
+++ src/app/components/input/index.tsx
@@ -2,8 +2,6 @@ import { Root } from "./Root";
 import { Element } from "./Element";
 import { Label } from "./Label";
 import { Group } from "./Group";
-import { Select } from "./Select/Native";
-import { Select as SelectWithCombobox } from "./Select/WithCombobox";
 import { Textarea } from "./Textarea";
 
-export { Root, Element, Label, Group, Select, SelectWithCombobox, Textarea };
+export { Root, Element, Label, Group, Textarea };
