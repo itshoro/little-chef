dc9ecf7bb574f8fa13d88445a8f692f8170ea2f0
Tim Nelke <tim.nelke@outlook.com>
2024-07-28 00:14:57 +0200

    scale down like counter span

--- src/app/recipes/[slug]/components/buttons/optimistic-like-button.tsx
+++ src/app/recipes/[slug]/components/buttons/optimistic-like-button.tsx
@@ -37,7 +37,7 @@ const OptimisticLikeButton = (props: OptimisticLikeButtonProps) => {
         >
           <path d="M2 6.342a3.375 3.375 0 0 1 6-2.088 3.375 3.375 0 0 1 5.997 2.26c-.063 2.134-1.618 3.76-2.955 4.784a14.437 14.437 0 0 1-2.676 1.61c-.02.01-.038.017-.05.022l-.014.006-.004.002h-.002a.75.75 0 0 1-.592.001h-.002l-.004-.003-.015-.006a5.528 5.528 0 0 1-.232-.107 14.395 14.395 0 0 1-2.535-1.557C3.564 10.22 1.999 8.558 1.999 6.38L2 6.342Z" />
         </svg>
-        <span className="text-sm font-semibold">{optimisticLikes.count}</span>
+        <span className="text-xs font-semibold">{optimisticLikes.count}</span>
 
         <span className="sr-only">Like</span>
       </div>
