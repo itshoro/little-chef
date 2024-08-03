fd376db12a558a51e8209c61cc85b4b7081de066
Tim Nelke <tim.nelke@outlook.com>
2024-08-03 22:49:49 +0200

    add remaining character count display to step item

--- src/app/components/input/textarea.tsx
+++ src/app/components/input/textarea.tsx
@@ -1,21 +1,25 @@
 "use client";
 
+import { forwardRef } from "react";
 import { useInputContext } from "./context";
 
 type TextareaProps = React.ComponentPropsWithoutRef<"textarea">;
 
-const Textarea = (props: TextareaProps) => {
-  const { name } = useInputContext(Textarea.name);
-  const _name = props.name ? `${name}.${props.name}` : name;
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (props, ref) => {
+    const { name } = useInputContext(Textarea.name);
+    const _name = props.name ? `${name}.${props.name}` : name;
 
-  return (
-    <textarea
-      {...props}
-      className="m-0.5 min-h-[3lh] w-full rounded-lg border-none bg-transparent p-1.5 font-medium outline-none transition focus:ring-transparent"
-      name={_name}
-      id={_name}
-    />
-  );
-};
+    return (
+      <textarea
+        ref={ref}
+        {...props}
+        className="m-0.5 min-h-[3lh] w-full rounded-lg border-none bg-transparent p-1.5 font-medium outline-none transition focus:ring-transparent"
+        name={_name}
+        id={_name}
+      />
+    );
+  },
+);
 
 export { Textarea };
--- src/app/recipes/components/recipe-form/elements/step/item.tsx
+++ src/app/recipes/components/recipe-form/elements/step/item.tsx
@@ -4,7 +4,7 @@ import * as Generator from "@/app/components/generator";
 import * as Input from "@/app/components/input";
 import { Trash } from "@/app/components/icon/trash";
 import { useInputContext } from "@/app/components/input/context";
-import { useDeferredValue, useState } from "react";
+import { useDeferredValue, useEffect, useRef, useState } from "react";
 import { CooklangPreview } from "@/app/recipes/[slug]/components/cooklang-preview";
 
 type StepGeneratorItemProps = {
@@ -53,6 +53,8 @@ type InputMaskProps = {
 };
 
 const InputMask = ({ uuid, order, value, onChange }: InputMaskProps) => {
+  const textRef = useRef<React.ElementRef<"textarea">>(null);
+
   return (
     <>
       <Input.Root name="step">
@@ -63,6 +65,7 @@ const InputMask = ({ uuid, order, value, onChange }: InputMaskProps) => {
           <StepCounterLabel>{order}</StepCounterLabel>
           <Input.Group>
             <Input.Textarea
+              ref={textRef}
               value={value}
               onChange={onChange}
               maxLength={140}
@@ -71,16 +74,65 @@ const InputMask = ({ uuid, order, value, onChange }: InputMaskProps) => {
             />
           </Input.Group>
         </Input.Root>
-        <Generator.Remove
-          className="mb-auto ml-4 grid place-items-center rounded p-2.5 text-stone-500 transition-colors hover:bg-stone-100 disabled:bg-stone-200"
-          uid={uuid}
-        >
-          <div title="Remove">
-            <Trash />
+        <div className="mb-auto ml-4 grid place-items-center">
+          <Generator.Remove
+            className="grid place-items-center rounded p-2.5 text-stone-500 transition-colors hover:bg-stone-100 disabled:bg-stone-200"
+            uid={uuid}
+          >
+            <div title="Remove">
+              <Trash />
+            </div>
+          </Generator.Remove>
+          <div className="mt-2 text-stone-400">
+            <CharacterCount textRef={textRef} />
           </div>
-        </Generator.Remove>
+        </div>
       </Input.Root>
     </>
+  );
+};
+
+const CharacterCount = ({
+  textRef,
+}: {
+  textRef: React.RefObject<React.ElementRef<"textarea">>;
+}) => {
+  const [remainingCharacters, setRemainingCharaters] = useState<number>(
+    Number.MAX_SAFE_INTEGER,
+  );
+  const maxLength = useRef(Number.MAX_SAFE_INTEGER);
+
+  useEffect(() => {
+    textRef.current?.addEventListener("input", (e) => {
+      const target = e.target as React.ElementRef<"textarea">;
+      setRemainingCharaters(target.maxLength - target.value.length);
+      maxLength.current = target.maxLength;
+    });
+  }, []);
+
+  return (
+    <span className="relative flex items-center justify-center">
+      <svg viewBox="0 0 20 20" className="size-7 -rotate-90 overflow-visible">
+        <circle
+          cx="50%"
+          cy="50%"
+          fill="none"
+          r="10"
+          stroke="currentColor"
+          stroke-dasharray="63"
+          stroke-dashoffset={
+            63 - (63 * remainingCharacters) / maxLength.current
+          }
+          stroke-linecap="round"
+          stroke-width="2"
+        ></circle>
+      </svg>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <span className="text-sm">
+          {remainingCharacters < 50 && remainingCharacters}
+        </span>
+      </div>
+    </span>
   );
 };
 
