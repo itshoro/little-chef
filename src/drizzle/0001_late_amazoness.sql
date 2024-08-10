89596e5c7baff2f14894e4ef526010442860afc2
Tim Nelke <tim.nelke@outlook.com>
2024-08-11 01:01:05 +0200

    fix migration issues caused by altering the table instead of dropping it

--- src/drizzle/0001_late_amazoness.sql
+++ src/drizzle/0001_late_amazoness.sql
@@ -1,6 +1,13 @@
-ALTER TABLE `collectionSubscriptions` RENAME TO `collectionSubscriptions_old`;
+CREATE TABLE `collectionRecipes_TMP` (
+	`collectionId` integer NOT NULL,
+	`recipeId` integer NOT NULL,
+	FOREIGN KEY (`collectionId`) REFERENCES `collections`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`recipeId`) REFERENCES `recipes`(`id`) ON UPDATE no action ON DELETE cascade
+);
 --> statement-breakpoint
-ALTER TABLE `collectionRecipes` RENAME TO `collectionRecipes_old`;
+INSERT INTO `collectionRecipes_TMP` SELECT * FROM `collectionRecipes`;
+--> statement-breakpoint
+DROP TABLE `collectionRecipes`;
 --> statement-breakpoint
 CREATE TABLE `collectionRecipes` (
 	`collectionId` integer NOT NULL,
@@ -8,6 +15,26 @@ CREATE TABLE `collectionRecipes` (
 	FOREIGN KEY (`collectionId`) REFERENCES `collections`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`recipeId`) REFERENCES `recipes`(`id`) ON UPDATE no action ON DELETE cascade
 );
+--> statement-breakpoint
+INSERT INTO `collectionRecipes` SELECT * FROM `collectionRecipes_TMP`;
+--> statement-breakpoint
+DROP TABLE `collectionRecipes_TMP`;
+--> statement-breakpoint
+
+
+
+CREATE TABLE `collectionSubscriptions_TMP` (
+	`collectionId` integer NOT NULL,
+	`userId` integer NOT NULL,
+	`role` text NOT NULL,
+	PRIMARY KEY(`collectionId`, `userId`),
+	FOREIGN KEY (`collectionId`) REFERENCES `collections`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `collectionSubscriptions_TMP` SELECT * FROM `collectionSubscriptions`;
+--> statement-breakpoint
+DROP TABLE `collectionSubscriptions`;
 --> statement-breakpoint
 CREATE TABLE `collectionSubscriptions` (
 	`collectionId` integer NOT NULL,
@@ -18,10 +45,7 @@ CREATE TABLE `collectionSubscriptions` (
 	FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-INSERT INTO `collectionRecipes` SELECT * FROM `collectionRecipes_old`;
+INSERT INTO `collectionSubscriptions` SELECT * FROM `collectionSubscriptions_TMP`;
 --> statement-breakpoint
-INSERT INTO `collectionSubscriptions` SELECT * FROM `collectionSubscriptions_old`;
+DROP TABLE `collectionSubscriptions_TMP`;
 --> statement-breakpoint
-DROP TABLE `collectionSubscriptions_old`;
---> statement-breakpoint
-DROP TABLE `collectionRecipes_old`;
--- src/drizzle/0002_majestic_puppet_master.sql
+++ src/drizzle/0002_majestic_puppet_master.sql
@@ -1,4 +1,16 @@
-ALTER TABLE `collections` RENAME TO `collections_old`;
+CREATE TABLE `collections_TMP` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`publicId` text NOT NULL,
+	`isCustom` integer,
+	`visibility` text NOT NULL,
+	`itemCount` integer DEFAULT 0 NOT NULL,
+	`name` text NOT NULL,
+	`slug` text NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `collections_TMP` SELECT * FROM `collections`;
+--> statement-breakpoint
+DROP TABLE `collections`;
 --> statement-breakpoint
 CREATE TABLE `collections` (
 	`id` integer PRIMARY KEY NOT NULL,
@@ -10,6 +22,6 @@ CREATE TABLE `collections` (
 	`slug` text NOT NULL
 );
 --> statement-breakpoint
-INSERT INTO `collections` SELECT * FROM `collections_old`;
+INSERT INTO `collections` SELECT * FROM `collections_TMP`;
 --> statement-breakpoint
-DROP TABLE `collections_old`;
+DROP TABLE `collections_TMP`;
