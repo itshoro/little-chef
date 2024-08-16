abfaf3237877d976069ee112b38b19b94c4287d8
Tim Nelke <tim.nelke@outlook.com>
2024-08-16 17:58:12 +0200

    improve optimistic evaluation - stop unnecessary re-renders - toggle pending icon when optimisitc update occurs - changed styling when active

--- src/app/recipes/[slug]/components/buttons/optimistic-like-button.tsx
+++ src/app/recipes/[slug]/components/buttons/optimistic-like-button.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useOptimistic, useState } from "react";
+import { useMemo, useOptimistic, useTransition } from "react";
 import { BaseButton } from "../../../../components/base-button";
+import { set } from "zod";
 
 type OptimisticLikeButtonProps = {
   count: number;
@@ -13,7 +14,7 @@ type OptimisticLikeButtonProps = {
 };
 
 const OptimisticLikeButton = (props: OptimisticLikeButtonProps) => {
-  const [optimisticLikes, dispatchToggleLike] = useOptimisticLikes(
+  const [pending, optimisticLikes, dispatchToggleLike] = useOptimisticLikes(
     props.count,
     props.isLiked,
     props.action,
@@ -22,25 +23,38 @@ const OptimisticLikeButton = (props: OptimisticLikeButtonProps) => {
   return (
     <BaseButton
       type="button"
-      disabled={!props.disabled || optimisticLikes.pending}
-      onClick={async () => {
-        await dispatchToggleLike();
-      }}
+      disabled={!props.disabled || pending}
+      onClick={dispatchToggleLike}
       data-liked={optimisticLikes.isLiked}
-      className="px-4 data-[liked=true]:bg-neutral-50 data-[liked=true]:text-red-400 data-[liked=true]:shadow-inner"
+      className="relative px-4 data-[liked=true]:border-rose-100 data-[liked=true]:bg-rose-50 data-[liked=true]:text-rose-400 data-[liked=true]:shadow-none"
     >
       <div className="flex items-center gap-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="size-4"
-        >
-          <path d="M2 6.342a3.375 3.375 0 0 1 6-2.088 3.375 3.375 0 0 1 5.997 2.26c-.063 2.134-1.618 3.76-2.955 4.784a14.437 14.437 0 0 1-2.676 1.61c-.02.01-.038.017-.05.022l-.014.006-.004.002h-.002a.75.75 0 0 1-.592.001h-.002l-.004-.003-.015-.006a5.528 5.528 0 0 1-.232-.107 14.395 14.395 0 0 1-2.535-1.557C3.564 10.22 1.999 8.558 1.999 6.38L2 6.342Z" />
-        </svg>
+        {pending ? (
+          <svg className="size-4 animate-spin" viewBox="0 0 20 20">
+            <circle
+              cy="50%"
+              cx="50%"
+              r={8}
+              strokeWidth={2}
+              strokeDasharray={64}
+              strokeDashoffset={24}
+              fill="transparent"
+              stroke="currentColor"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="size-4"
+          >
+            <path d="M2 6.342a3.375 3.375 0 0 1 6-2.088 3.375 3.375 0 0 1 5.997 2.26c-.063 2.134-1.618 3.76-2.955 4.784a14.437 14.437 0 0 1-2.676 1.61c-.02.01-.038.017-.05.022l-.014.006-.004.002h-.002a.75.75 0 0 1-.592.001h-.002l-.004-.003-.015-.006a5.528 5.528 0 0 1-.232-.107 14.395 14.395 0 0 1-2.535-1.557C3.564 10.22 1.999 8.558 1.999 6.38L2 6.342Z" />
+          </svg>
+        )}
         <span className="text-xs font-semibold">{optimisticLikes.count}</span>
 
-        <span className="sr-only">Like</span>
+        <span className="sr-only">Likes</span>
       </div>
     </BaseButton>
   );
@@ -51,33 +65,32 @@ function useOptimisticLikes(
   isLiked: OptimisticLikeButtonProps["isLiked"],
   action: OptimisticLikeButtonProps["action"],
 ) {
-  const [likes, setLikes] = useState({
-    count: count,
-    isLiked: isLiked,
-  });
-
-  const [optimisticLikes, setOptimisticLikes] = useOptimistic(
-    likes,
-    (state, action: "add" | "remove") => {
-      switch (action) {
-        case "add": {
-          return { count: state.count + 1, isLiked: true };
-        }
-        case "remove": {
-          return { count: state.count - 1, isLiked: false };
-        }
-      }
-    },
+  const likes = useMemo(
+    () => ({
+      count,
+      isLiked,
+    }),
+    [count, isLiked],
   );
+  const [optimisticLikes, setOptimisticLikes] = useOptimistic(likes);
+  const [pending, startTransition] = useTransition();
 
-  async function dispatch() {
-    const actionType = optimisticLikes.isLiked ? "remove" : "add";
-    setOptimisticLikes(actionType);
-    const serverResult = await action(actionType);
-    setLikes({ ...serverResult });
+  function dispatch() {
+    startTransition(async () => {
+      const actionType = optimisticLikes.isLiked ? "remove" : "add";
+      if (optimisticLikes)
+        setOptimisticLikes(({ count, isLiked }) => ({
+          isLiked: !isLiked,
+          count: isLiked ? count - 1 : count + 1,
+        }));
+      await new Promise((resolve) =>
+        setTimeout(() => resolve(undefined), 1000),
+      );
+      await action(actionType);
+    });
   }
 
-  return [optimisticLikes, dispatch] as const;
+  return [pending, optimisticLikes, dispatch] as const;
 }
 
 export { OptimisticLikeButton };
