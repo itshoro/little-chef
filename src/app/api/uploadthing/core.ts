d74412e53055a87760f7d6084377c2e88a7607fd
Tim Nelke <tim.nelke@outlook.com>
2024-08-31 23:52:30 +0200

    add uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,21 @@
+import { validateRequest } from "@/lib/auth/lucia";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "1MB" } })
+    .middleware(async ({ req }) => {
+      const { user } = await validateRequest();
+
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
