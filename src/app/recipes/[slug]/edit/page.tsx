c87d833272af07242fd2a04360e5683fa50366eb
Tim Nelke <tim.nelke@outlook.com>
2024-11-26 22:20:20 +0100

    fix label

--- src/app/recipes/[slug]/edit/page.tsx
+++ src/app/recipes/[slug]/edit/page.tsx
@@ -55,7 +55,7 @@ const EditRecipePage = async (props: EditRecipePageProps) => {
                 >
                   <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
                 </svg>
-                Add Recipe
+                Update Recipe
               </div>
             </SubmitWithPending>
           </div>
