f9b40c4135c2fb2a0d0464af5de4c218501ef523
Tim Nelke <tim.nelke@outlook.com>
2024-12-22 21:25:37 +0100

    umlauts are now handled when generating slugs

--- src/lib/slug.ts
+++ src/lib/slug.ts
@@ -11,6 +11,10 @@ export function generateSlug(name: string) {
   return name
     .toLowerCase()
     .trim()
+    .replace(/ä/g, "ae")
+    .replace(/ö/g, "oe")
+    .replace(/ü/g, "ue")
+    .replace(/ß/g, "ss")
     .replace(/[^\w\s-]/g, "")
     .replace(/\s+/g, "-")
     .slice(0, 50);
