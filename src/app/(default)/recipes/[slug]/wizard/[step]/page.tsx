b579f3f5607f1c5aaf596a0cc52a773575f2117a
Tim Nelke <tim.nelke@outlook.com>
2025-04-29 20:47:44 +0200

    feat: add wake lock

--- src/app/(default)/recipes/[slug]/wizard/[step]/page.tsx
+++ src/app/(default)/recipes/[slug]/wizard/[step]/page.tsx
@@ -1,8 +1,9 @@
+import { ForceWakeLock } from "@/app/components/wake-lock/force-wakelock";
+import { validateRequest } from "@/lib/auth";
 import { getRecipe, getRecipeSteps } from "@/lib/dal/recipe";
 import { extractParts } from "@/lib/slug";
-import { WizardStep } from "../step";
 import { Actions } from "../actions";
-import { validateRequest } from "@/lib/auth";
+import { WizardStep } from "../step";
 
 type PageProps = {
   params: Promise<{
@@ -30,26 +31,29 @@ const Page = async (props: PageProps) => {
     Number(searchParams.servings) / recipe.recommendedServingSize;
 
   return (
-    <div>
-      <div className="mt-auto flex flex-col items-center justify-center py-8">
-        <WizardStep
-          description={displayedStep.description}
-          ingredientScaleFactor={ingredientScaleFactor}
-        />
-        <div className="mt-4 text-sm">
-          Step {step + 1} of {steps.length}
+    <>
+      <ForceWakeLock />
+      <div>
+        <div className="mt-auto flex flex-col items-center justify-center py-8">
+          <WizardStep
+            description={displayedStep.description}
+            ingredientScaleFactor={ingredientScaleFactor}
+          />
+          <div className="mt-4 text-sm">
+            Step {step + 1} of {steps.length}
+          </div>
         </div>
+        <section className="flex justify-between border-t border-stone-100 py-4 dark:border-stone-800">
+          <Actions
+            slug={recipe.slug}
+            publicId={recipe.publicId}
+            stepCount={steps.length}
+            step={step}
+            servings={searchParams.servings}
+          />
+        </section>
       </div>
-      <section className="flex justify-between border-t border-stone-100 py-4 dark:border-stone-800">
-        <Actions
-          slug={recipe.slug}
-          publicId={recipe.publicId}
-          stepCount={steps.length}
-          step={step}
-          servings={searchParams.servings}
-        />
-      </section>
-    </div>
+    </>
   );
 };
 
