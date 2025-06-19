3ee9ab2a9baf1e16b38367860df827da2bbed27b
Tim Nelke <tim.nelke@outlook.com>
2025-06-19 20:53:12 +0200

    use `@libsql` instead of `better-sqlite3` due to async transaction returns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  serverExternalPackages: ["better-sqlite3", "sharp"],
+  serverExternalPackages: ["sharp"],
   images: {
     remotePatterns: [
       {
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,8 +1,8 @@
-import sqlite from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
-const sql = sqlite(process.env.DATABASE_URL);
+const sql = createClient({ url: `file:${process.env.DATABASE_URL}` });
 const db = drizzle(sql, { schema });
 
 export { db };
