ce7a07d1c5141793b4eaddde81dfd2c5cc94efb1
Tim Nelke <tim.nelke@outlook.com>
2026-02-13 22:34:54 +0100

    adjust border for search results

--- src/components/ui/link-card/root.tsx
+++ src/components/ui/link-card/root.tsx
@@ -4,7 +4,7 @@ type CardProps = {
 
 const Root = ({ children }: CardProps) => {
   return (
-    <article className="relative rounded-xl bg-stone-100 shadow-xs ring-1 dark:bg-stone-800 dark:ring-white/10">
+    <article className="relative rounded-xl bg-stone-100 shadow-xs ring-1 ring-black/10 dark:bg-stone-800 dark:ring-white/10">
       {children}
     </article>
   );
